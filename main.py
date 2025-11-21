42dfe503c48c6529d2fe880f79d1a714feb512ae
PhileasCONTATBRIERE <phileascontatbriere@gmail.com>
2025-11-21 16:53:28 +0100

    Fixed text: screen.blit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ clock = pygame.time.Clock()
 
 # Remplit l’écran en noir une seule fois
 screen.fill((0, 0, 0))
-pygame.display.update()
 
 # Boucle minimale : uniquement pour fermer avec Échap
 running = True
@@ -18,13 +17,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((0, 0, 0))
+
     if pygame.font:
         font = pygame.font.Font(None, 64)
         text = font.render("Worms", True, (255, 255, 255))
         background.blit(text, (0,0))
+    
+    screen.blit(background, (0, 0))
+    pygame.display.update()
     clock.tick(60)  # limits FPS to 60
             
 
