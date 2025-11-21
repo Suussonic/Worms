2fc5bef02af33dbe03c457051a8fb0a9245a7316
Suussonic <150890672+Suussonic@users.noreply.github.com>
2025-11-21 16:11:21 +0100

    Ajout du Frame Rate a 60FPS

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ pygame.init()
 
 # Plein écran noir
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-pygame.display.set_caption("Fullscreen Black Screen")
+pygame.display.set_caption("Worms")
+clock = pygame.time.Clock()
 
 # Remplit l’écran en noir une seule fois
 screen.fill((0, 0, 0))
@@ -16,5 +17,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+    clock.tick(60)  # limits FPS to 60
+            
 
 pygame.quit()
