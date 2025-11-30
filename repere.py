24076b7c400be2452a8153e9461dc716f09bfd7a
LEJEUNE Thomas <thomas2004.lejeune@gmail.com>
2025-11-30 18:07:18 +0100

    Déplacement Worm

---
+++ repere.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Worm:
+    def __init__(self, x, y, width=20, height=40):
+        self.rect = pygame.Rect(x, y, width, height)
+        
+        self.velocity = pygame.math.Vector2(0, 0)
+        
+        self.SPEED = 5          # Vitesse horizontale
+        self.JUMP_FORCE = -15   # Force du saut
+        self.GRAVITY = 0.8      # Gravité
+        
+        self.on_ground = False
+
+    def handle_input(self):
+        keys = pygame.key.get_pressed()
+
+        # Gauche / Droite
+        if keys[pygame.K_LEFT] or keys[pygame.K_q]:
+            self.velocity.x = -self.SPEED
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.velocity.x = self.SPEED
+        else:
+            self.velocity.x = 0
+
+        # Saut (seulement si on est au sol)
+        if (keys[pygame.K_SPACE] or keys[pygame.K_z] or keys[pygame.K_UP]) and self.on_ground:
+            self.jump()
+
+    def jump(self):
+        self.velocity.y = self.JUMP_FORCE
+        self.on_ground = False
+
+    def update(self, screen_height):
+        self.velocity.y += self.GRAVITY
+
+        self.rect.x += self.velocity.x
+        self.rect.y += self.velocity.y
+
+        # Gestion collision sol (pour l'instant bord de l'écran)
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
+            self.velocity.y = 0
+            self.on_ground = True
+        else:
+            self.on_ground = False
+
+    def get_position(self):
+        return (self.rect.x, self.rect.y)
